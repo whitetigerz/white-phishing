6fb880ed07a4669ed3c62bfc8ef3c1f3e9221214
whitetigerz <50932142+whitetigerz@users.noreply.github.com>
2019-10-27 04:47:14 -0700

    Create wtph.sh

---
+++ wtph.sh
@@ -0,0 +1,16 @@
+cd
+cd white-phishing
+apt update
+apt upgrade
+apt install git
+apt install php
+apt install python
+apt install ruby
+apt install figlet
+gem install lolcat
+echo -e "\033[1m \e[32 SHELL EXECUTING......please wait" |lolcat|lolcat
+echo -e "\033[1m \e[32 script executing" |lolcat|lolcat
+echo -e "\033[1m \e[32 DONT USE THIS FOR ILLEGAL PURPOSE" |lolcat|lolcat
+echo -e "\033[1m \e[32 SCRIPT WAS MADE BY PRINCE YASIN" |lolcat|lolcat
+figlet WT-PHISH
+php -S localhost:4449/index.html
